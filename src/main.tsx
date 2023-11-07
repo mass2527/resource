8a0c2d3235026812cf785ee2e9149dfe5dfec298
Dongho Kim <dongre2527@gmail.com>
2023-11-07 22:04:48 +0900

    feat: react-query 셋업

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { worker } from "./mocks/browser.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 if (import.meta.env.DEV) {
   worker.start();
 }
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
