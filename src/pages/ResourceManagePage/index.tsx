782e36300e3dfefa3f3963a6c6074fb1e9f55619
Dongho Kim <dongre2527@gmail.com>
2023-11-09 16:09:41 +0900

    fix: ResourceList overflow 이슈 수정

--- src/pages/ResourceManagePage/index.tsx
+++ src/pages/ResourceManagePage/index.tsx
@@ -9,13 +9,13 @@ import QueryErrorResetBoundary from "../../components/QueryErrorResetBoundary";
 export default function ResourceManagePage() {
   return (
     <div className="min-h-screen flex">
-      <aside className="max-w-sm w-full flex flex-col border-r">
+      <aside className="max-w-sm w-full border-r">
         <div className="h-14 border-b p-2 flex gap-2 justify-between">
           <UrlResourceCreateButton />
           <ImageResourceCreateButton />
         </div>
 
-        <div className="p-2 flex-1 h-[calc(100vh - 14rem)] overflow-y-auto">
+        <div className="p-2 h-[calc(100vh-56px)] overflow-y-auto">
           <QueryErrorResetBoundary>
             <Suspense fallback={<ResourceListPlaceholder />}>
               <ResourceList />
